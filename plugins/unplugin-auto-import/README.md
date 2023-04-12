ae6ca2c125587218a6a3ad5e1fd74382f9fab853
hjp <jp.huang@hongzhengtech.com>
2023-04-12 09:20:52 +0800

    ✨ feat(unplugin-auto-import/unplugin-vue-components):

---
+++ plugins/unplugin-auto-import/README.md
@@ -0,0 +1,9 @@
+## main
+
+试用 `unplugin-auto-import` 和 `unplugin-vue-components` 。
+
+## case
+
+- 类型
+- 自定义组件
+- 插件
---
+++ plugins/unplugin-auto-import/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const counter: Ref<number> = ref(0);
+</script>
+
+<template>
+  <CompHeader />
+  <CompContent>
+    <button @click="counter++">count is: {{ counter }}</button>
+  </CompContent>
+  <CompFooter />
+</template>
